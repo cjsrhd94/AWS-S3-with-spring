b377023d95e9e7f0c15bae522c8ef8b03c38dc90
cjsrhd94 <cjsrhd94@gmail.com>
2023-03-07 12:45:26 +0900

    init

---
+++ src/main/java/com/cjsrhd94/S3/S3Application.java
@@ -0,0 +1,13 @@
+package com.cjsrhd94.S3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3Application.class, args);
+	}
+
+}
